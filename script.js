5c9c92df62adee67963ddee1336868f9c11a07b4
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-02-19 17:23:12 +0600

    play again

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const diceEl = document.querySelector('.dice');
 
 const rollEl = document.querySelector('.btn--roll');
 const holdEl = document.querySelector('.btn--hold');
-const newEl = document.querySelector('btn--new');
+const newEl = document.querySelector('.btn--new');
 
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
@@ -15,9 +15,6 @@ const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
 let scores, currentScore, activePlayer, playing;
-
-
-
 
 
 const inIt = function () {
@@ -29,14 +26,11 @@ const inIt = function () {
 
     score0El.textContent = 0;
     score1El.textContent = 0;
-
+    console.log('hhhhhh');
     diceEl.classList.add('hidden');
 
     player0El.classList.add('player--active');
     player1El.classList.remove('player--active');
-
-
-
 }
 
 inIt();
@@ -74,31 +68,30 @@ rollEl.addEventListener('click', function () {
 
 holdEl.addEventListener('click', function () {
 
-     if(playing){
+    if (playing) {
 
-    
+
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent =
-        scores[activePlayer];
-       
+            scores[activePlayer];
 
-        if(scores[activePlayer]>=100){
 
-            playing=false;
+        if (scores[activePlayer] >= 100) {
+
+            playing = false;
             rollEl.classList.add('hidden');
 
             document.querySelector(`.player--${activePlayer}`)
-            .classList.add('player--winner');
+                .classList.add('player--winner');
 
             document.querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+                .classList.remove('player--active');
         }
-        else
-        {
+        else {
             switchPlayer();
         }
-    
-     }
 
-    
+    }
 })
+
+newEl.addEventListener('click', inIt);
